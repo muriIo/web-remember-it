7848070a6afc7a1c334fc891376057d6aa0c5448
Nicolas Nagano <nicolasjap@hotmail.com>
2024-08-09 10:24:17 -0300

    chore: page register with libs

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,185 +1,143 @@
-import React from "react";
+import { useForm } from "react-hook-form";
+import "../register/styles.sass";
+import { UserSchema } from "../../validation/user-schema";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { GiCheckMark } from "react-icons/gi";
+import { RxCross1 } from "react-icons/rx";
+import { useState } from "react";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 const Register = () => {
+  // const [password, setPassword] = useState<string>("");
+  const [type, setType] = useState<string>("password");
+
+  const useFormMethods = useForm<UserSchema.CreateData>({
+    mode: "all",
+    reValidateMode: "onSubmit",
+    resolver: yupResolver(UserSchema.registerData),
+  });
+
+  const {
+    watch,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useFormMethods;
+
+  const onSubmit = (data: UserSchema.CreateData) => {
+    console.log(data);
+  };
+
+  const handleToggle = () => {
+    setType(type === "password" ? "text" : "password");
+  };
+
+  const passwordValue = watch("password", "");
+  const isLongEnough = passwordValue.length >= 6;
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(passwordValue);
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        backgroundColor: "white",
-        borderRadius: "8px",
-        width: "100%",
-        maxWidth: "600px",
-        height: "80%",
-        padding: "20px",
-        boxShadow: "0 4px 4px rgba(0, 0, 0, 0.5)",
-        margin: "0 auto",
-      }}
-    >
-      <div
-        style={{
-          flexDirection: "column",
-          display: "flex",
-          padding: "10px",
-          flex: "1",
-          minWidth: "200px",
-        }}
-      >
-        <label style={{ marginBottom: "10px", color: "#696969" }}>
-          Name
-          <input
-            style={{
-              width: "100%",
-              marginBottom: "10px",
-              borderRadius: "4px",
-              borderWidth: "1px",
-              padding: "6px",
-            }}
-          />
-        </label>
-        <label style={{ marginBottom: "10px", color: "#696969" }}>
-          Birthday
-          <input
-            type="date"
-            style={{
-              width: "100%",
-              marginBottom: "10px",
-              borderColor: "#696969",
-              borderRadius: "4px",
-              borderWidth: "1px",
-              padding: "6px",
-            }}
-          />
-        </label>
-        <label style={{ marginBottom: "10px", color: "#696969" }}>
-          E-Mail
-          <input
-            type="email"
-            style={{
-              width: "100%",
-              marginBottom: "10px",
-              borderRadius: "4px",
-              borderWidth: "1px",
-              padding: "6px",
-            }}
-          />
-        </label>
-        <label style={{ marginBottom: "10px", color: "#696969" }}>
-          Password
-          <input
-            type="password"
-            style={{
-              width: "100%",
-              marginBottom: "10px",
-              borderRadius: "4px",
-              borderWidth: "1px",
-              padding: "6px",
-            }}
-          />
-        </label>
-        <label style={{ marginBottom: "10px", color: "#696969" }}>
-          Confirm Password
-          <input
-            type="password"
-            style={{
-              width: "100%",
-              marginBottom: "10px",
-              borderRadius: "4px",
-              borderWidth: "1px",
-              padding: "6px",
-            }}
-          />
-        </label>
-      </div>
-
-      {/* Divider */}
-      <div
-        style={{
-          width: "1px",
-          backgroundColor: "#9A9A89",
-          margin: "0 20px",
-        }}
-      />
-
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          flex: "1",
-          minWidth: "200px",
-          textAlign: "center",
-        }}
-      >
-        <img
-          src={"/images/register.png"}
-          alt="Register"
-          style={{ width: "100%", height: "auto", maxWidth: "200px" }}
-        />
-        <a
-          // href="/login"
-          style={{
-            textAlign: "center",
-            marginTop: "10px",
-            marginBottom: "20px",
-            fontWeight: "bold",
-            textDecoration: "underline",
-            color: "black",
-            cursor: "pointer",
-            fontSize: "11px",
-          }}
-        >
-          <text style={{ textAlign: "center" }}>I already have an account</text>
-        </a>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "baseline",
-            gap: "3px",
-            textAlign: "center",
-          }}
-        >
-          <input type="checkbox" id="terms" />
-          <label htmlFor="terms" style={{ fontSize: "11px" }}>
-            I agree to the
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="register-container">
+        <div className="register-column">
+          <label aria-invalid={!!errors.name}>
+            Name
+            <input type="text" {...register("name")} />
+            {errors.name && (
+              <span className="error-message">{errors.name.message}</span>
+            )}
           </label>
-
-          <a
-            href="#"
-            style={{
-              marginTop: "10px",
-              marginBottom: "20px",
-              fontWeight: "bold",
-              textDecoration: "underline",
-              color: "black",
-              cursor: "pointer",
-              fontSize: "10px",
-            }}
-          >
-            <text style={{ fontSize: "11px" }}>Terms of Service</text>
-          </a>
+          <label>
+            Birthday
+            <input type="date" {...register("birthday")} />
+            {errors.birthday && (
+              <span className="error-message">{errors.birthday.message}</span>
+            )}
+          </label>
+          <label>
+            E-Mail
+            <input type="email" {...register("email")} />
+            {errors.email && (
+              <span className="error-message">{errors.email.message}</span>
+            )}
+          </label>
+          <label>
+            Password
+            <div className="password-container">
+              <input type={type} {...register("password")} />
+              <i onClick={handleToggle}>
+                {type === "password" ? (
+                  <FaRegEyeSlash size={20} />
+                ) : (
+                  <FaRegEye size={20} />
+                )}
+              </i>
+            </div>
+            <div className="validation-status">
+              {isLongEnough ? (
+                <GiCheckMark
+                  color={"green"}
+                  size={"15px"}
+                  style={{ marginRight: "6px" }}
+                />
+              ) : (
+                <RxCross1
+                  color="red"
+                  size={"15px"}
+                  style={{ marginRight: "6px" }}
+                />
+              )}{" "}
+              Password must be longer than 6 characters
+            </div>
+            <div className="validation-status">
+              {hasSpecialChar ? (
+                <GiCheckMark
+                  color={"green"}
+                  size={"15px"}
+                  style={{ marginRight: "6px" }}
+                />
+              ) : (
+                <RxCross1
+                  color="red"
+                  size={"15px"}
+                  style={{ marginRight: "6px" }}
+                />
+              )}{" "}
+              Password must contain special characters
+            </div>
+          </label>
+          <label>
+            Confirm Password
+            <input type="password" {...register("confirm_password")} />
+            {errors.confirm_password && (
+              <span className="error-message">
+                {errors.confirm_password.message}
+              </span>
+            )}
+          </label>
         </div>
 
-        <button
-          style={{
-            fontWeight: "bold",
-            borderWidth: "1px",
-            borderColor: "#333333",
-            marginTop: "20px",
-            padding: "10px 20px",
-            borderRadius: "8px",
-            backgroundColor: "#9A9A89",
-            color: "white",
-            cursor: "pointer",
-            width: "100%",
-          }}
-        >
-          Register
-        </button>
+        <div className="divider" />
+
+        <div className="register-right">
+          <img src={"/images/register.png"} alt="Register" />
+          <a className="link">
+            <span>I already have an account</span>
+          </a>
+          <div className="terms">
+            <input type="checkbox" id="terms" {...register("terms")} />
+            <label htmlFor="terms">I agree to the</label>
+            <a href="#">
+              <span>Terms of Service</span>
+            </a>
+          </div>
+          {errors.terms && (
+            <span className="error-message">{errors.terms.message}</span>
+          )}
+          <button type="submit">Register</button>
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
 
